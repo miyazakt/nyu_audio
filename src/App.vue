c09fa335de1e108405e617f7a7ed664f1f130359
Taisuke Miyazaki <MiyazakiTaisuke@lifull.com>
2017-12-05 16:32:53 +0900

    パネルコンポーネントを作成

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
-    <Board/>
+    <panel
+      name="Icon"
+      image="icon_rss.svg"
+      @click="this.onClick" ></panel>
   </div>
 </template>
 
 <script>
 import Board from './components/Board'
+import Panel from './components/Panel'
 
 export default {
   name: 'app',
+  methods: {
+    onClick: function (event) {
+    }
+  },
   components: {
-    Board
+    'board': Board,
+    'panel': Panel
   }
 }
 </script>
---
+++ src/components/Panel.vue
@@ -0,0 +1,48 @@
+<template>
+  <div 
+    class="panel"
+    draggable="true"
+    @click="handleClick"
+    @dragstart="handleDragStart"
+    @dragend="handleDragEnd"
+    @dragenter="handleDragEnter" >
+    <span class="title">{{ this.name }}</span>
+    <img :src="imagePath" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    name: String,
+    image: String
+  },
+  computed: {
+    imagePath: function () {
+      return '/static/img/' + this.image
+    }
+  },
+  methods: {
+    handleClick: function (event) {
+      this.$emit('click', event)
+    },
+    handleDragStart: function (event) {
+      this.$emit('dragstart', event)
+    },
+    handleDragEnd: function (event) {
+      this.$emit('dragend', event)
+    },
+    handleDragEnter: function (event) {
+      this.$emit('dragenter', event)
+    }
+  }
+}
+</script>
+<style scoped>
+.panel {
+  display: inline-block;
+}
+.title {
+  display: block;
+}
+</style>
